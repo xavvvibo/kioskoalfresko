14d9d9a1c4513163943080ca09db6c4ed00fcd6a
Javier Bocanegra <xavvvibo@gmail.com>
2026-04-23 08:18:49 +0200

    fix smash image path to png

--- content/menu.ts
+++ content/menu.ts
@@ -15,7 +15,7 @@ export const smashPromo: MenuPromo = {
   price: "14,00€",
   variants: ["Ahumada", "Baiconera", "Cabra Loca"],
   image: {
-    src: "/menu/smash-burgers-hero.jpg",
+    src: "/menu/smash-burgers-hero.png",
     alt: "Smash burgers de Kiosko Alfresko con patatas",
   },
 };
